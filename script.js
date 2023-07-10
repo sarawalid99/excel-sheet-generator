c84c1e7f1434fbf2a3dd6fc2c92fc07174bd597d
Sara_11 <92929722+sarawalid99@users.noreply.github.com>
2023-07-10 03:20:02 +0300

    SweetAlert.js added

--- script.js
+++ script.js
@@ -4,24 +4,42 @@ columns = document.getElementsByClassName("columns")[0]
 tableExists = false
 
 const generateTable = () => {
-    let rowsNumber = parseInt(rows.value), columnsNumber = parseInt(columns.value)
-    table.innerHTML = ""
-    for(let i=0; i<rowsNumber; i++){
-        var tableRow = ""
-        for(let j=0; j<columnsNumber; j++){
-            tableRow += `<td contenteditable></td>`
+    let rowsNumber = parseInt(rows.value);
+    let columnsNumber = parseInt(columns.value);
+
+    if (rowsNumber > 0 && columnsNumber > 0) {
+        table.innerHTML = "";
+
+        for (let i = 0; i < rowsNumber; i++) {
+            let tableRow = "";
+            for (let j = 0; j < columnsNumber; j++) {
+                tableRow += `<td contenteditable></td>`;
+            }
+            table.innerHTML += tableRow;
         }
-        table.innerHTML += tableRow
-    }
-    if(rowsNumber>0 && columnsNumber>0){
-        tableExists = true
+
+        tableExists = true;
+    } else {
+        // Display an alert using SweetAlert.js when the fields are empty
+        Swal.fire({
+            icon: 'error',
+            title: 'Empty Fields',
+            text: 'Please enter a valid number of rows and columns.',
+        });
     }
 }
 
 const ExportToExcel = (type, fn, dl) => {
-    if(!tableExists){
-        return
+    if (!tableExists) {
+        // Display an alert using SweetAlert.js when there is no generated table
+        Swal.fire({
+            icon: 'error',
+            title: 'No Table to Export',
+            text: 'Please generate a table before exporting.',
+        });
+        return;
     }
+
     var elt = table
     var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" })
     return dl ? XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' })
